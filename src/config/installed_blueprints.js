86e4029b809e360ef07bc95ca6e838fcc70f8b25
Crowdbotics <team@crowdbotics.com>
2020-08-22 20:31:47 +0000

    remove package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -2,7 +2,6 @@ export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'Settings93131', human_name: 'Settings', access_route: 'Settings93131'},
 { name: 'UserProfile93124', human_name: 'User Profile', access_route: 'UserProfile93124'},
-{ name: 'Settings93123', human_name: 'Settings', access_route: 'Settings93123'},
 { name: 'Settings93121', human_name: 'Settings', access_route: 'Settings93121'},
 { name: 'SignIn293119', human_name: 'Sign In 2', access_route: 'SignIn293119'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,7 +7,6 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import Settings93131Navigator from '../features/Settings93131/navigator';
 import UserProfile93124Navigator from '../features/UserProfile93124/navigator';
-import Settings93123Navigator from '../features/Settings93123/navigator';
 import Settings93121Navigator from '../features/Settings93121/navigator';
 import SignIn293119Navigator from '../features/SignIn293119/navigator';
 
@@ -20,7 +19,6 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 Settings93131: { screen: Settings93131Navigator },
 UserProfile93124: { screen: UserProfile93124Navigator },
-Settings93123: { screen: Settings93123Navigator },
 Settings93121: { screen: Settings93121Navigator },
 SignIn293119: { screen: SignIn293119Navigator },
 
